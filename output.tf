484397dc9c0dce2a2abec8673cdd34c1b98eddbc
Jon Benson <bensojon@gmail.com>
2017-12-27 09:54:05 -0200

    Fix output warnings

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "iam_role_id" {
-  value = "${aws_iam_role.consul.id}"
+  value = "${aws_iam_role.consul.*.id}"
 }
 
 output "instance_profile_id" {
-  value = "${aws_iam_instance_profile.consul.id}"
+  value = "${aws_iam_instance_profile.consul.*.id}"
 }
